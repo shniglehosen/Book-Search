eee7b97a495b22ecec482de7d5661c5c22047892
Nathan Aramyan <nathan.aramyan@gmail.com>
2022-12-05 15:49:40 -0700

    refactored client side to support graphql

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,13 +2,19 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
+
+// add apollo graphql 
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../utils/mutations";
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+
+  // define login mutation
+  const [loginUser] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -26,13 +32,13 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      // call login mutation
+      const { data } = await loginUser({
+        variables: { ...userFormData },
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      const { token, user } = data.login;
 
-      const { token, user } = await response.json();
       console.log(user);
       Auth.login(token);
     } catch (err) {
@@ -41,7 +47,6 @@ const LoginForm = () => {
     }
 
     setUserFormData({
-      username: '',
       email: '',
       password: '',
     });
